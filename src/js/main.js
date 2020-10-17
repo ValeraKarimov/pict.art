f7a075198f0fc1ebcd22cd9631ba57a85ab0c3af
Valerii Karimov <valeraguess@gmail.com>
2020-10-18 00:44:17 +0300

    add new modules

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,11 @@ import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
+import calc from './modules/calc';
+import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
+import burger from './modules/burger';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
@@ -15,5 +20,10 @@ window.addEventListener('DOMContentLoaded', () => {
   mask('[name="phone"]');
   checkTextInputs('[name="name"]');
   checkTextInputs('[name="message"]');
-  showMoreStyles('.button-styles','.styles-2');
+  showMoreStyles('.button-styles','#styles .row');
+  calc('#size', '#material', '#options', '.promocode', '.calc-price');
+  filter();
+  pictureSize('.sizes-block');
+  accordion('.accordion-heading');
+  burger('.burger-menu', '.burger');
 });
